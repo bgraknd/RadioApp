3aa54a84ba23d967c3c947b714dc5e0ba8f179da
bgraknd <bgraknd@gmail.com>
2019-11-09 03:39:32 +0300

    fragment components created

---
+++ app/src/main/java/com/example/spotifycloneapp/favorites/FavoritesFragment.kt
@@ -0,0 +1,24 @@
+package com.example.spotifycloneapp.favorites
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.spotifycloneapp.R
+
+class FavoritesFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_favorites, container, false)
+    }
+
+    companion object {
+        fun newInstance() = FavoritesFragment()
+    }
+
+}
