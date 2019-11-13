b2d8dde6c96ef01935337edd4ced84994d3debbb
bgraknd <bgraknd@gmail.com>
2019-11-13 16:27:14 +0300

    fetch radios

---
+++ app/src/main/java/com/example/spotifycloneapp/RadioServiceProvider.kt
@@ -0,0 +1,17 @@
+package com.example.spotifycloneapp
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RadioServiceProvider {
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BuildConfig.SERVER_URL)
+        .build()
+
+    private val apiService = retrofit.create(SpotifyApiService::class.java)
+
+    fun getRadioService(): SpotifyApiService = apiService
+
+}
---
+++ app/src/main/java/com/example/spotifycloneapp/SpotifyApiService.kt
@@ -0,0 +1,13 @@
+package com.example.spotifycloneapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface SpotifyApiService {
+    @GET("popularRadios.json")
+    fun getPopularRadios(): Call<List<Radio>>
+
+    @GET("locationRadios.json")
+    fun getLocationRadios(): Call<List<Radio>>
+
+}
