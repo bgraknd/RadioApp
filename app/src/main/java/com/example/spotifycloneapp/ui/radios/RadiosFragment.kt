1e478bde245f257511b8a72b17661bcb75fe865d
bugra <bgraknd@gmail.com>
2019-12-09 15:17:59 +0300

    add databinding and create recyclerviews

--- app/src/main/java/com/example/spotifycloneapp/ui/radios/RadiosFragment.kt
+++ app/src/main/java/com/example/spotifycloneapp/ui/radios/RadiosFragment.kt
@@ -2,27 +2,40 @@ package com.example.spotifycloneapp.ui.radios
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.spotifycloneapp.R
 import com.example.spotifycloneapp.data.RadioDataSource
+import com.example.spotifycloneapp.data.Status.LOADING
+import com.example.spotifycloneapp.data.Status.SUCCESS
+import com.example.spotifycloneapp.databinding.FragmentRadiosBinding
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class RadiosFragment : Fragment() {
 
+    private lateinit var binding: FragmentRadiosBinding
+
     val radioDataSource = RadioDataSource()
+
+    private val popularRadiosAdapter = RadiosAdapter()
+    private val locationRadiosAdapter = RadiosAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_radios, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_radios, container, false)
+
+        binding.recyclerViewPopular.adapter = popularRadiosAdapter
+        binding.recyclerViewLocation.adapter = locationRadiosAdapter
+
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -44,12 +57,31 @@ class RadiosFragment : Fragment() {
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe {
-                Log.v(
-                    "TEST",
-                    it.popularRadios.status.toString() + " " + it.locationRadios.status.toString()
-                )
+                renderUI(it)
             }
+    }
 
+    private fun renderUI(radiosFragmentViewState: RadiosFragmentViewState) {
+
+        when (radiosFragmentViewState.popularRadios.status) {
+            SUCCESS -> {
+                binding.progressPopularRadios.visibility = View.GONE
+                popularRadiosAdapter.setRadioList(radiosFragmentViewState.popularRadios.data!!)
+            }
+            LOADING -> {
+                binding.progressPopularRadios.visibility = View.VISIBLE
+            }
+        }
+
+        when (radiosFragmentViewState.locationRadios.status) {
+            SUCCESS -> {
+                binding.progressLocationRadios.visibility = View.GONE
+                locationRadiosAdapter.setRadioList(radiosFragmentViewState.locationRadios.data!!)
+            }
+            LOADING -> {
+                binding.progressLocationRadios.visibility = View.VISIBLE
+            }
+        }
     }
 
     companion object {
